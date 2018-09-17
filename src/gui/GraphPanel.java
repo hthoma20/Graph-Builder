5533f95b9c1959f089b9b0b4376da68dca318e73
hthoma20 <thoma20@up.edu>
2018-09-17 01:08:22 -0700

    Added scalable coloring. New colors will be added to the color map as necessary.

--- src/gui/GraphPanel.java
+++ src/gui/GraphPanel.java
@@ -6,6 +6,7 @@ import graph.Vertex;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,8 +44,7 @@ public class GraphPanel extends JPanel {
         Map<Vertex, Integer> coloring= graph.getColoring();
 
         for(Vertex v : graph.getVertexSet()){
-            System.out.println("Coloring " + coloring.get(v) + " " + colorMap.get(coloring.get(v)));
-            g.setColor(colorMap.get(coloring.get(v)));
+            g.setColor(intToColor(coloring.get(v)));
             fillCircle(v.getX(),v.getY(),vRad,g);
         }
 
@@ -67,12 +67,67 @@ public class GraphPanel extends JPanel {
 
     private void setupColorMap(){
         this.colorMap= new HashMap<>();
-        colorMap.put(0,new Color(255,0,0));
-        colorMap.put(1,new Color(0,255,0));
-        colorMap.put(2,new Color(0,0,255));
-        colorMap.put(3,new Color(255,255,0));
-        colorMap.put(4,new Color(255,0,255));
-        colorMap.put(5,new Color(0,255,255));
+        this.colorMap.put(0, hColor(0));
+        this.colorMap.put(1, hColor(120));
+        this.colorMap.put(2, hColor(240));
+        this.colorMap.put(3, hColor(60));
+        this.colorMap.put(4, hColor(180));
+        this.colorMap.put(5, hColor(300));
+    }
+
+    private Color intToColor(int x){
+        if(colorMap.containsKey(x)){
+            return colorMap.get(x);
+        }
+
+        int[] hArray= new int[colorMap.size()];
+        int index= 0;
+        for(Color c : colorMap.values()){
+            hArray[index++]= colorH(c);
+        }
+        Arrays.sort(hArray);
+
+        //the largest arc distance in degrees
+        int maxDist= 0;
+        //index where the largest arc starts
+        int arcStart= 0;
+        for(int i=0; i < hArray.length-1; i++){
+            int dist= hArray[i+1]-hArray[i];
+            if(dist > maxDist){
+                maxDist= dist;
+                arcStart= i;
+            }
+        }
+        int dist= 360 - hArray[hArray.length-1];
+        if(dist > maxDist){
+            maxDist= dist;
+            arcStart= hArray.length-1;
+        }
+
+        Color newColor= hColor(hArray[arcStart]+maxDist/2);
+
+        colorMap.put(x,newColor);
+        return newColor;
+    }
+
+    /**
+     * find the hue angle of a color
+     *
+     * @param c the color
+     * @return the hue in range [0,360)
+     */
+    private int colorH(Color c){
+        float hVal= Color.RGBtoHSB(c.getRed(),c.getBlue(),c.getGreen(),null)[0];
+        return (int)(hVal*360);
+    }
+
+    /**
+     *
+     * @param h the hue angle in range [0,360)
+     * @return the color with h and full s and v
+     */
+    private Color hColor(int h){
+        return Color.getHSBColor(1-(float)h/360,1,1);
     }
 
     public void setVRad(int vRad) {
